963862f0ee88201f9cb6e00474593d7b5822ca32
Bhavana Srinivas <bhavana@pubnubs-MacBook-Air-94.local>
2015-06-09 13:57:26 -0700

    adding pi-pubnub

---
+++ pi.py
@@ -0,0 +1,13 @@
+from Pubnub import Pubnub
+from iotconnector import iotbridge
+ 
+pi = iotbridge(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
+ 
+channel = 'iotchannel'
+message = "hello from pi"
+ 
+def callbackfn(m, n):
+  print(m)
+ 
+pi.send(channel, message)
+pi.connect(channel, callbackfn)
